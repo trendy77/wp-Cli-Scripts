2ca3ec6375b955a626bfe9143ce34e9ee580631f
The Hon Rev Dr Trent D.A.M-D. Fisher, Esq. DFA* <tfisher811@gmail.com>
2017-09-24 07:37:00 +1000

    tidy

--- setup_tidyingDb/menuSetup.php
+++ setup_tidyingDb/menuSetup.php
@@ -8,20 +8,25 @@ $aliaie=array('@fnr','@us','@spec','@style','@gov','@glo','@orgbizes','@tp','@va
 	
 		echo ' At site ';
 		$input=array();
+			$input[$inputNum++] = ('post create --post_type=page --post_title=Contact --post_status=publish --field=ID --format=ids');
 		
-			$input[$inputNum++] = 'post create --post_type=page --post_title=Home --post_status=publish --field=ID --format=ids)';
+			$input[$inputNum++] = ('post create --post_type=page --post_title=Home --post_status=publish --field=ID --format=ids');
 			$input[$inputNum++] = ('menu create "Main Navigation"');
+					$input[$inputNum++] = ('item add-custom main-navigation ' .$TIT.' ' . $ADDY .' ');
+					$input[$inputNum++] = ('item add-custom main-navigation ' .$TIT.' ' . $ADDY .' ');
 			$input[$inputNum++] = ('menu location assign main-navigation primary');
+				$input[$inputNum++] = ('menu create "Catgory Navigation"');
+					$input[$inputNum++] = ('menu create "Catgory Navigation"');
 			$input[$inputNum++] = ('option update posts_per_page 6');
 			$input[$inputNum++] = ('option update show_on_front "page"');  
 			$input[$inputNum++] = ('option update page_on_front $(wp ' .$alias .' post list --post_type=page --post_status=publish --posts_per_page=1 --pagename=home --field=ID --format=ids');
-			$input[$inputNum++] = ('menu create "Catgory Navigation"');
+			
 			$input[$inputNum++] = ('menu location assign category-navigation top');
 			#$input[$inputNum++] = ('menu add ' dfsdfsd
 			#$input[$inputNum++] = ('menu l'
 			#$input[$inputNum++] = ('menu l'
 		
-			$input[$inputNum++] = ('menu location assign category-navigation top');
+			$input[$inputNum++] = ('menu location assign category-nav top');
 			#$input[$inputNum++] = ('menu add ' dfsdfsd
 			#$input[$inputNum++] = ('menu l'
 			#$input[$inputNum++] = ('menu l'
